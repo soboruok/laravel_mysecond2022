8e7529eb63f7bdf5e3e43c009153b0a1ae52f450
Hee <smpartlittlepuppy@gmail.com>
2022-06-26 01:19:53 +1000

    keep old data in the form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -9,31 +9,31 @@
                 {{-- stops cross-site scripting attacks --}}
                 @csrf
                 <div class="form-control">
-                    Title:<input type="text" name="title">
+                    Title:<input type="text" name="title" value="{{old('title')}}" />
                     @error('title') <p class="red">{{ $message }}</p> @enderror
                 </div>
                 <div class="form-control">
-                    Tags (Comma Separated):<input type="text" name="tags">
+                    Tags (Comma Separated):<input type="text" name="tags" value="{{old('tags')}}" />
                     @error('tags') <p class="red">{{ $message }}</p> @enderror
                 </div>
                 <div class="form-control">
-                    Company:<input type="text" name="company">
+                    Company:<input type="text" name="company" value="{{old('company')}}" />
                     @error('company') <p class="red">{{ $message }}</p> @enderror
                 </div>
                 <div class="form-control">
-                    Location:<input type="text" name="location">
+                    Location:<input type="text" name="location" value="{{old('location')}}" />
                     @error('location') <p class="red">{{ $message }}</p> @enderror
                 </div>
                 <div class="form-control">
-                    Email:<input type="email" name="email">
+                    Email:<input type="email" name="email" value="{{old('email')}}" />
                     @error('email') <p class="red">{{ $message }}</p> @enderror
                 </div>
                 <div class="form-control">
-                    Website:<input type="text" name="website">
+                    Website:<input type="text" name="website" value="{{old('website')}}" />
                     @error('website') <p class="red">{{ $message }}</p> @enderror
                 </div>
                 <div class="form-control">
-                    Description:<textarea cols="50" rows="10" name="description"></textarea>
+                    Description:<textarea cols="50" rows="10" name="description">{{old('location')}}</textarea>
                     @error('description') <p class="red">{{ $message }}</p> @enderror
                 </div>
                 <input type="submit" value="Send" class="btn btn-primary" />
